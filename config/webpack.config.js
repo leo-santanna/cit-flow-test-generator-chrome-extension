cc8c1e80d0cbfd28b79d184dd924e05c168a6371
dlopespg <jimrazak@gmail.com>
2025-02-13 13:08:13 -0300

    Restructure to use webpack

---
+++ config/webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { merge } = require('webpack-merge');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const webpack = require('webpack');
+
+const common = require('./webpack.common.js');
+const PATHS = require('./paths');
+
+// Merge webpack configuration files
+const config = (env, argv) =>
+  merge(common, {
+    entry: {
+      popup: PATHS.src + '/popup.js',
+      contentScript: PATHS.src + '/contentScript.js',
+      background: PATHS.src + '/background.js',
+      minifyHtml: PATHS.src + '/minifyHtml.js',
+    },
+    devtool: argv.mode === 'production' ? false : 'source-map',
+    plugins: [
+      new NodePolyfillPlugin(),
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
+    ],
+  });
+
+module.exports = config;
